483801e504069e345f086c3aef9674d9561a1d3b
Jesus <llius619@gmail.com>
2022-04-05 16:37:46 +0200

    chore(christmas-lights-kata): Apply prettier formatter

--- src/christmas-lights-kata/christmas-lights-kata.test.ts
+++ src/christmas-lights-kata/christmas-lights-kata.test.ts
@@ -1,59 +1,78 @@
-import { Lights } from "./utils/Lights";
+import { Lights } from './utils/Lights';
 
 const gridWithPosition = [
-  [ [ '2,0', 'X' ], [ '2,1', 'X' ], [ '2,2', 'X' ] ],
-  [ [ '1,0', 'X' ], [ '1,1', 'X' ], [ '1,2', 'X' ] ],
-  [ [ '0,0', 'X' ], [ '0,1', 'X' ], [ '0,2', 'X' ] ]
-]
+  [
+    ['2,0', 'X'],
+    ['2,1', 'X'],
+    ['2,2', 'X'],
+  ],
+  [
+    ['1,0', 'X'],
+    ['1,1', 'X'],
+    ['1,2', 'X'],
+  ],
+  [
+    ['0,0', 'X'],
+    ['0,1', 'X'],
+    ['0,2', 'X'],
+  ],
+];
 
-const grid = [ [ 'X', 'X', 'X' ], [ 'X', 'X', 'X' ], [ 'X', 'X', 'X' ] ]
-const gridLightsOn = [ [ 'O', 'O', 'O' ], [ 'O', 'O', 'O' ], [ 'O', 'O', 'O' ] ]
+const grid = [
+  ['X', 'X', 'X'],
+  ['X', 'X', 'X'],
+  ['X', 'X', 'X'],
+];
+const gridLightsOn = [
+  ['O', 'O', 'O'],
+  ['O', 'O', 'O'],
+  ['O', 'O', 'O'],
+];
 
 describe('christmas-lights-kata', () => {
-    test('Grid generator with position', async () => {
-      const lights = new Lights()
-      lights.generateLightGrid(3,3)
-      const lightsGridWithPosition = lights.getGridWithPosition()
+  test('Grid generator with position', async () => {
+    const lights = new Lights();
+    lights.generateLightGrid(3, 3);
+    const lightsGridWithPosition = lights.getGridWithPosition();
 
-      expect(lightsGridWithPosition).toEqual(gridWithPosition)
-    });
+    expect(lightsGridWithPosition).toEqual(gridWithPosition);
+  });
 
-    test('Grid generator', async () => {
-      const lights = new Lights()
-      lights.generateLightGrid(3,3)
-      const lightsGrid = lights.getGrid()
+  test('Grid generator', async () => {
+    const lights = new Lights();
+    lights.generateLightGrid(3, 3);
+    const lightsGrid = lights.getGrid();
 
-      expect(lightsGrid).toEqual(grid)
-    });
+    expect(lightsGrid).toEqual(grid);
+  });
 
-    test(`
+  test(`
       GIVEN i have 3,3 grid
       WHEN i turn on 3,3 through 0,0
       THEN every light should be on
     `, () => {
-      // GIVEN
-      const lights = new Lights()
-      lights.generateLightGrid(3,3)
-      // WHEN
-      lights.toggleLightsFrom(0,0,3,3)
-      // THEN
-      const grid = lights.getGrid()
-      expect(grid).toEqual(gridLightsOn)
-    })
+    // GIVEN
+    const lights = new Lights();
+    lights.generateLightGrid(3, 3);
+    // WHEN
+    lights.toggleLightsFrom(0, 0, 3, 3);
+    // THEN
+    const grid = lights.getGrid();
+    expect(grid).toEqual(gridLightsOn);
+  });
 
-    test(`
+  test(`
       GIVEN i have 3,3 grid
       WHEN i turn on 0,0 through 3,3
       THEN every light should be on
     `, () => {
-      // GIVEN
-      const lights = new Lights()
-      lights.generateLightGrid(3,3)
-      // WHEN
-      lights.toggleLightsFrom(3,3,0,0)
-      // THEN
-      const grid = lights.getGrid()
-      expect(grid).toEqual(gridLightsOn)
-  })
+    // GIVEN
+    const lights = new Lights();
+    lights.generateLightGrid(3, 3);
+    // WHEN
+    lights.toggleLightsFrom(3, 3, 0, 0);
+    // THEN
+    const grid = lights.getGrid();
+    expect(grid).toEqual(gridLightsOn);
+  });
 });
-  
--- src/christmas-lights-kata/utils/Lights.ts
+++ src/christmas-lights-kata/utils/Lights.ts
@@ -1,50 +1,57 @@
 export class Lights {
+  private grid: [[string]];
+  private gridWithPosition: [[[string, string]]];
 
-    private grid: [[string]]
-    private gridWithPosition : [[[string, string]]]
+  constructor() {
+    this.grid = [['']];
+    this.gridWithPosition = [[['', '']]];
+  }
 
-    constructor(){
-        this.grid = [['']]
-        this.gridWithPosition =  [[['', '']]]
+  public generateLightGrid(rows: number, columns: number) {
+    for (let indexRows = 0; indexRows < rows; indexRows++) {
+      this.grid[indexRows] = [''];
+      this.gridWithPosition[indexRows] = [['', '']];
+      for (let indexColumns = 0; indexColumns < columns; indexColumns++) {
+        this.grid[indexRows][indexColumns] = 'X';
+        this.gridWithPosition[indexRows][indexColumns] = [
+          indexRows + ',' + indexColumns,
+          'X',
+        ];
+      }
     }
+    this.gridWithPosition.reverse();
+  }
 
-    public generateLightGrid(rows: number, columns: number){
-        for (let indexRows = 0; indexRows < rows; indexRows++) {
-            this.grid[indexRows] = ['']
-            this.gridWithPosition[indexRows] = [['', '']]
-            for (let indexColumns = 0; indexColumns < columns; indexColumns++) {
-                this.grid[indexRows][indexColumns] = 'X'
-                this.gridWithPosition[indexRows][indexColumns] = [indexRows + ',' + indexColumns ,'X']
-            }
+  public getGridWithPosition() {
+    return this.gridWithPosition;
+  }
+  public getGrid() {
+    return this.grid;
+  }
+
+  public toggleLightsFrom(
+    startX: number,
+    startY: number,
+    endX: number,
+    endY: number,
+  ): void {
+    if (startX < endX && startY < endY) {
+      endX--;
+      endY--;
+      for (let indexRows = startX; indexRows <= endX; indexRows++) {
+        for (let indexColumns = startY; indexColumns <= endY; indexColumns++) {
+          this.grid[indexRows][indexColumns] = 'O';
         }
-        this.gridWithPosition.reverse()
+      }
     }
-
-    public getGridWithPosition(){
-        return this.gridWithPosition
-    }
-    public getGrid(){
-        return this.grid
-    }
-
-    public toggleLightsFrom(startX: number, startY: number, endX: number, endY: number): void {
-        if(startX < endX && startY < endY){
-            endX--;
-            endY--;
-            for (let indexRows = startX; indexRows <= endX; indexRows++) {
-                for (let indexColumns = startY; indexColumns <= endY; indexColumns++) {
-                        this.grid[indexRows][indexColumns] = 'O'                    
-                   }
-            }       
+    if (startX > endX && startY > endY) {
+      startX--;
+      startY--;
+      for (let indexRows = endX; indexRows <= startX; indexRows++) {
+        for (let indexColumns = endY; indexColumns <= startY; indexColumns++) {
+          this.grid[indexRows][indexColumns] = 'O';
         }
-        if(startX > endX && startY > endY){
-            startX--;
-            startY--;
-            for (let indexRows = endX; indexRows <= startX; indexRows++) {
-                for (let indexColumns = endY; indexColumns <= startY; indexColumns++) {
-                        this.grid[indexRows][indexColumns] = 'O'                    
-                   }
-            }       
-        }
+      }
     }
+  }
 }
