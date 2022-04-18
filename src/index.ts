5eb8d20d7cb6efaa261466f914fc773007f00e9d
Jesus <llius619@gmail.com>
2022-04-18 10:23:32 +0200

    feat: Improve base code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello world!');
+import { Application, Request, Response } from 'express';
+const express = require('express');
+const app = express();
+const port = process.env.NODE_DOCKER_PORT;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
